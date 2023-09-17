7311671bfc8440a86693fbbb4c28e44d877a7ff0
smalicev <73667854+smalicev@users.noreply.github.com>
2023-09-17 10:36:21 -0400

    Delete Arrays directory

--- Arrays/exerciseseven.js
+++
@@ -1,8 +0,0 @@
-let array = [3, 5, 7];
-console.log(sumOfSquares(array)); // => 83
-
-function sumOfSquares(array){
- return array.reduce(function(total, currentValue) {
-  return total + currentValue*currentValue;
- }, 0) 
-}
--- Arrays/exercisetwo.js
+++
@@ -1,8 +0,0 @@
-let myArray = [1, 3, 6, 11, 4, 2,
-  4, 9, 17, 16, 0];
-
-  function logEven(array) {
-    console.log(array.filter(element => (element % 2) === 0));
-  }
-
-  logEven(myArray);
