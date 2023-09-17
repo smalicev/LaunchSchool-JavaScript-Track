a5059aad78e92a9239cebc1cb96dd258d5a3bf07
smalicev <73667854+smalicev@users.noreply.github.com>
2023-09-17 10:35:59 -0400

    Delete Functions directory

--- Functions/multiply.js
+++
@@ -1,9 +0,0 @@
-let rlSync = require('readline-sync')
-
-function getMultiply() {
-  let firstNumber = rlSync.question("Enter the first number:");
-  let secondNumber = rlSync.question("Enter the second number:");
-  console.log(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`)
-};
-
-getMultiply();
